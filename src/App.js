81ced138f28e8076578d7836fa582d271841c64e
Nielll951 <rodina9180gaocheng@ukr.net>
2023-12-30 21:54:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState, useLayoutEffect } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useLayoutEffect(() => {
+    console.log(count)
+  }, [count])
+
+  return (
+    <>
+      <button onClick={() => setCount(c => c + 1)}>Increment</button>
+      <div>{count}</div>
+    </>
+  );
+}
+
+export default App;
